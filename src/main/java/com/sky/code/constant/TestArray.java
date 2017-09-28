d1152874342e96a22213f0e01f034ff8ad97b95c
ejunjsh <sjj050121014@163.com>
2017-09-28 11:43:18 +0800

    test constant

---
+++ src/main/java/com/sky/code/constant/TestArray.java
@@ -0,0 +1,14 @@
+package com.sky.code.constant;
+
+
+public class TestArray {
+
+    public static void main(String[] args){
+        int[] a={1,2,3};
+        int[] b={1,2,3};
+
+        if(a==b){
+            System.out.println("constants is equal.");
+        }
+    }
+}
