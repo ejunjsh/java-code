01eda29a602b41e4dd3ebc065064a26a45523e11
zhouff <sjj050121014@163.com>
2018-08-04 22:56:12 +0800

    minor changes

---
+++ src/main/java/com/sky/code/ratelimiter/RateLimiterService.java
@@ -0,0 +1,18 @@
+package com.sky.code.ratelimiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+public class RateLimiterService {
+
+
+    public RateLimiterService(double num){
+        rateLimiter=RateLimiter.create(num);
+    }
+
+    RateLimiter rateLimiter ;
+
+
+    public boolean tryAcquire(){
+        return rateLimiter.tryAcquire();
+    }
+}
---
+++ src/main/java/com/sky/code/ratelimiter/RateLimiterTest.java
@@ -0,0 +1,16 @@
+package com.sky.code.ratelimiter;
+
+import java.util.Date;
+
+public class RateLimiterTest {
+
+    public static void main(String[] args) {
+        RateLimiterService service = new RateLimiterService(5.0);
+
+        while (true) {
+            if(service.tryAcquire()){
+                System.out.println(new Date());
+            }
+        }
+    }
+}
